0c72ae57612322bbd2995282190413503ea5cee8
william19maran <william19.maran@gmail.com>
2025-04-28 18:19:37 +0200

    creazione del programma springboot che farà da web service

---
+++ spring/prog_terminal_marittimo/src/main/java/progetto_terminal_marittimo/prog_terminal_marittimo/ProgTerminalMarittimoApplication.java
@@ -0,0 +1,13 @@
+package progetto_terminal_marittimo.prog_terminal_marittimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgTerminalMarittimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgTerminalMarittimoApplication.class, args);
+	}
+
+}
---
+++ spring/prog_terminal_marittimo/src/test/java/progetto_terminal_marittimo/prog_terminal_marittimo/ProgTerminalMarittimoApplicationTests.java
@@ -0,0 +1,13 @@
+package progetto_terminal_marittimo.prog_terminal_marittimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgTerminalMarittimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
